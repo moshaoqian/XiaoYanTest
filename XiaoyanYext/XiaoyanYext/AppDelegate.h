a13636d79042939134494110110cc7c181920276
moshaoqian <moshaoqian@live.com>
2017-08-09 13:23:02 +0800

    测试提交

---
+++ XiaoyanYext/XiaoyanYext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoyanYext
+//
+//  Created by benjamin on 2017/8/9.
+//  Copyright © 2017年 benjamintext. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
